1d7b05cdd0845acb41bde5b479f1fc574db88308
Erica Greene <ericagreene@gmail.com>
2017-07-14 11:02:34 -0400

    Fix typescript errors that were breaking the build

--- packages/backend-api/src/test/test_helper.ts
+++ packages/backend-api/src/test/test_helper.ts
@@ -41,7 +41,7 @@ import {
 const TEST_ENVS = ['test', 'circle_ci'];
 
 function isTestEnv() {
-  return TEST_ENVS.indexOf(process.env.NODE_ENV) > -1;
+  return TEST_ENVS.indexOf(process.env.NODE_ENV || '') > -1;
 }
 
 logger.setTestMode(isTestEnv());
