342c77c2d8beb46d6739f073ffdfacd49ba6012a
Rufus Hamade <rufus.hamade@gmail.com>
2018-12-17 08:54:42 +0000

    frontend: more local store fixes.

--- packages/frontend-web/src/app/platform/localStore.ts
+++ packages/frontend-web/src/app/platform/localStore.ts
@@ -16,19 +16,53 @@ limitations under the License.
 
 import { RESTRICT_TO_SESSION } from '../config';
 
-const LOCAL_STORAGE_TOKEN_KEY = 'moderator/auth_token';
-const storage = () => RESTRICT_TO_SESSION ? sessionStorage : localStorage;
+const LOCAL_STORAGE_AUTH_TOKEN_KEY = 'moderator/auth_token';
+const storage = () => {
+  if (typeof localStorage === 'undefined') {
+    return {} as any;
+  }
+  if (RESTRICT_TO_SESSION) {
+    return sessionStorage;
+  }
+  return localStorage;
+};
 
 export function getToken(): string | undefined {
-  return storage()[LOCAL_STORAGE_TOKEN_KEY];
+  return storage()[LOCAL_STORAGE_AUTH_TOKEN_KEY];
 }
 
 export function saveToken(token: string): string {
   if (token) {
-    storage()[LOCAL_STORAGE_TOKEN_KEY] = token;
+    storage()[LOCAL_STORAGE_AUTH_TOKEN_KEY] = token;
   } else {
-    delete storage()[LOCAL_STORAGE_TOKEN_KEY];
+    delete storage()[LOCAL_STORAGE_AUTH_TOKEN_KEY];
   }
 
   return token;
+}
+
+function versionKey(key: string) {
+  return `moderator/${key}-version`;
+}
+function dataKey(key: string) {
+  return `moderator/${key}-data`;
+}
+
+export function getStoreItem(key: string, version: number) {
+  const versionToCheck = storage()[versionKey(key)];
+  if (!versionToCheck || parseInt(versionToCheck, 10) !== version) {
+    return null;
+  }
+
+  const data = storage()[dataKey(key)];
+  if (!data) {
+    return null;
+  }
+
+  return data;
+}
+
+export function saveStoreItem(key: string, version: number, data: string) {
+  storage()[versionKey(key)] = version;
+  storage()[dataKey(key)] = data;
 }
