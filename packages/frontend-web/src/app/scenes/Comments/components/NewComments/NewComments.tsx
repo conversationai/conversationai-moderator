conversationai/conversationai-moderator

commit 870d17e291938855c5b976f25c4d438c01fcafc1

frontend: use serverside values for Rule.action.
Also tighten type checking in ModerateButtons.

packages/frontend-web/src/app/scenes/Comments/components/NewComments/NewComments.tsx
@@ -24,9 +24,9 @@ import {
   IArticleModel,
   ICommentDatedModel,
   ICommentModel,
-  ICommentScoredModel,
+  ICommentScoredModel, IRuleAction,
   IRuleModel,
-  ITagModel,
+  ITagModel, RULE_ACTION_ACCEPT, RULE_ACTION_DEFER, RULE_ACTION_HIGHLIGHT, RULE_ACTION_REJECT,
   TagModel,
 } from '../../../../../models';
 import { ICommentAction } from '../../../../../types';
@@ -301,7 +301,7 @@ export interface INewCommentsState {
   isNavStuck?: boolean;
   isConfirmationModalVisible?: boolean;
   isRuleInfoVisible?: boolean;
-  confirmationAction?: ICommentAction;
+  confirmationAction?: IRuleAction;
   actionCount?: number;
   actionText?: string;
   toastButtonLabel?: 'Undo';
@@ -945,16 +945,16 @@ export class NewComments extends React.Component<INewCommentsProps, INewComments
     }
   }
 
-  matchAction(action: ICommentAction) {
+  matchAction(action: IRuleAction) {
     let showActionIcon;
 
-    if (action === 'approve') {
+    if (action === RULE_ACTION_ACCEPT) {
       showActionIcon = <ApproveIcon {...css({ fill: DARK_COLOR })} />;
-    } else if (action === 'reject') {
+    } else if (action === RULE_ACTION_REJECT) {
       showActionIcon = <RejectIcon {...css({ fill: DARK_COLOR })} />;
-    } else if (action === 'highlight') {
+    } else if (action === RULE_ACTION_HIGHLIGHT) {
       showActionIcon = <HighlightIcon {...css({ fill: DARK_COLOR })} />;
-    } else if (action === 'defer') {
+    } else if (action === RULE_ACTION_DEFER) {
       showActionIcon = <DeferIcon {...css({ fill: DARK_COLOR })} />;
     }
 
@@ -962,7 +962,7 @@ export class NewComments extends React.Component<INewCommentsProps, INewComments
   }
 
   @autobind
-  triggerActionToast(action: ICommentAction, count: number, callback: (action?: ICommentAction) => any) {
+  triggerActionToast(action: IRuleAction, count: number, callback: (action?: IRuleAction) => any) {
     this.setState({
       isConfirmationModalVisible: true,
       confirmationAction: action,
@@ -1017,7 +1017,7 @@ export class NewComments extends React.Component<INewCommentsProps, INewComments
   @autobind
   onTagButtonClick(tagId: string) {
     const ids = this.getSelectedIDs();
-    this.triggerActionToast('tag', ids.length, () => this.props.tagComments(ids, tagId));
+    this.triggerActionToast('Accept', ids.length, () => this.props.tagComments(ids, tagId));
     this.toggleTaggingToolTip();
   }
 

packages/frontend-web/src/app/components/LazyLoadComment/LazyLoadComment.tsx
@@ -20,7 +20,11 @@ import { List } from 'immutable';
 import React from 'react';
 const Linkify = require('react-linkify').default;
 import { ICommentModel, ITagModel } from '../../../models';
-import { IConfirmationAction, ITopScore } from '../../../types';
+import {
+  IConfirmationAction,
+  IModerationAction,
+  ITopScore,
+} from '../../../types';
 import {
   Avatar,
   Link,
@@ -115,7 +119,7 @@ export class BasicBody extends React.PureComponent<IBasicBodyProps, IBasicBodySt
     });
   }
 
-  getActiveButtons(): List<IConfirmationAction> {
+  getActiveButtons(): List<IModerationAction> {
     const { comment, tagRejectionModalVisible } = this.props;
     const shouldDisplayReject = tagRejectionModalVisible && tagRejectionModalVisible.id === comment.id && tagRejectionModalVisible.isVisible;
 
@@ -126,7 +130,7 @@ export class BasicBody extends React.PureComponent<IBasicBodyProps, IBasicBodySt
     if (comment.isHighlighted) { activeCommentStates.push('highlight'); }
     if (comment.isDeferred) { activeCommentStates.push('defer'); }
 
-    return List<IConfirmationAction>(activeCommentStates);
+    return List<IModerationAction>(activeCommentStates);
   }
 
   isModerated() {
@@ -140,7 +144,7 @@ export class BasicBody extends React.PureComponent<IBasicBodyProps, IBasicBodySt
 
     const activeCommentStates = this.getActiveButtons();
 
-    const shouldReset = this.isModerated() && activeCommentStates.includes(action);
+    const shouldReset = this.isModerated() && activeCommentStates.includes(action as IModerationAction);
 
     // Highlight action already contains special reset function
     const newAction = shouldReset
@@ -236,7 +240,7 @@ export class BasicBody extends React.PureComponent<IBasicBodyProps, IBasicBodySt
       output.push(addRange(comment.text, currentIndex, topScore.end, true));
       output.push(addRange(comment.text, currentIndex, comment.text.length));
     } else {
-      output = [<span>{comment.text}</span>];
+      output = [<span key={comment.id}>{comment.text}</span>];
     }
 
     return(

packages/frontend-web/src/app/components/ModerateButtons/ModerateButtons.tsx
@@ -17,7 +17,7 @@ limitations under the License.
 import { autobind } from 'core-decorators';
 import { List } from 'immutable';
 import React from 'react';
-import { IConfirmationAction } from '../../../types';
+import { IModerationAction } from '../../../types';
 import {
   GUTTER_DEFAULT_SPACING,
   LIGHT_PRIMARY_TEXT_COLOR,
@@ -84,16 +84,12 @@ export interface IModerateButtonsProps {
   hideLabel?: boolean;
   vertical?: boolean;
   darkOnLight?: boolean;
-  onClick?(action: IConfirmationAction): any;
+  onClick?(action: IModerationAction): any;
   containerSize?: number;
-  activeButtons?: List<IConfirmationAction>;
+  activeButtons?: List<IModerationAction>;
   disabled?: boolean;
   requireReasonForReject?: boolean;
   onRejectWithTag?(): any;
-}
-
-export interface IModerateButtonsState {
-  tooltipVisible: boolean;
 }
 
 export class ModerateButtons

packages/frontend-web/src/app/components/ModerateButtons/ModerateButtonsStory.tsx
@@ -17,8 +17,8 @@ limitations under the License.
 import { storiesOf } from '@storybook/react';
 
 import { List } from 'immutable';
-import { IConfirmationAction } from '../../../types';
-import { ModerateButtons } from '../ModerateButtons';
+import { IModerationAction } from '../../../types';
+import { ModerateButtons } from './ModerateButtons';
 
 storiesOf('ModerateButtons', module)
   .add('Horizontal', () => (
@@ -38,7 +38,7 @@ storiesOf('ModerateButtons', module)
   .add('Vertical Approve', () => (
     <div>
       <ModerateButtons
-        activeButtons={List(['approve']) as List<IConfirmationAction>}
+        activeButtons={List(['approve']) as List<IModerationAction>}
         vertical
       />
     </div>
@@ -46,7 +46,7 @@ storiesOf('ModerateButtons', module)
   .add('Vertical Reject', () => (
     <div>
       <ModerateButtons
-        activeButtons={List(['reject']) as List<IConfirmationAction>}
+        activeButtons={List(['reject']) as List<IModerationAction>}
         vertical
       />
     </div>
@@ -54,7 +54,7 @@ storiesOf('ModerateButtons', module)
   .add('Vertical Highlight', () => (
     <div>
       <ModerateButtons
-        activeButtons={List(['approve', 'highlight']) as List<IConfirmationAction>}
+        activeButtons={List(['approve', 'highlight']) as List<IModerationAction>}
         vertical
       />
     </div>
@@ -62,7 +62,7 @@ storiesOf('ModerateButtons', module)
   .add('Vertical Defer', () => (
     <div>
       <ModerateButtons
-        activeButtons={List(['defer']) as List<IConfirmationAction>}
+        activeButtons={List(['defer']) as List<IModerationAction>}
         vertical
       />
     </div>

packages/frontend-web/src/app/scenes/Comments/components/CommentDetail/CommentDetail.tsx
@@ -32,6 +32,7 @@ import {
 } from '../../../../../models';
 import {
   IConfirmationAction,
+  IModerationAction,
 } from '../../../../../types';
 import {
   Arrow,
@@ -329,7 +330,7 @@ export interface ICommentDetailState {
     top: number,
     left: number,
   };
-  activeButtons?: List<IConfirmationAction>;
+  activeButtons?: List<IModerationAction>;
   upArrowIsFocused?: boolean;
   downArrowIsFocused?: boolean;
   infoIconFocused?: boolean;
@@ -961,7 +962,7 @@ export class CommentDetail extends React.Component<ICommentDetailProps, IComment
   }
 
   @autobind
-  async moderateComment(action: IConfirmationAction) {
+  async moderateComment(action: IModerationAction) {
     const isModerated = this.props.comment.isModerated;
     const shouldResetAction = this.state.activeButtons && this.state.activeButtons.includes(action);
     const commentAction: IConfirmationAction = shouldResetAction ? 'reset' : action;
@@ -1099,7 +1100,7 @@ export class CommentDetail extends React.Component<ICommentDetailProps, IComment
     this.setState({ isConfirmationModalVisible: false });
   }
 
-  getActiveButtons(props: ICommentDetailProps): List<IConfirmationAction> {
+  getActiveButtons(props: ICommentDetailProps): List<IModerationAction> {
     const comment: ICommentModel = props.comment;
     if (!comment) { return null; }
     let activeButtons = List();
@@ -1109,7 +1110,7 @@ export class CommentDetail extends React.Component<ICommentDetailProps, IComment
     if (comment.isHighlighted) { activeButtons = activeButtons.push('highlight'); }
     if (comment.isDeferred) { activeButtons = List(['defer']); }
 
-    return activeButtons as List<IConfirmationAction>;
+    return activeButtons as List<IModerationAction>;
   }
 
   @autobind

packages/frontend-web/src/app/scenes/Settings/Settings.tsx
@@ -27,14 +27,19 @@ import FileCopyIcon from '@material-ui/icons/FileCopy';
 
 import {
   CategoryModel,
-  ICategoryModel, IPreselectModel,
-  IRuleModel, ITaggingSensitivityModel,
-  ITagModel, IUserModel,
+  ICategoryModel,
+  IPreselectModel,
+  IRuleAction,
+  IRuleModel,
+  ITaggingSensitivityModel,
+  ITagModel,
+  IUserModel,
   PreselectModel,
+  RULE_ACTION_ACCEPT,
   RuleModel,
-  TaggingSensitivityModel, TagModel,
+  TaggingSensitivityModel,
+  TagModel,
 } from '../../../models';
-import { IConfirmationAction } from '../../../types';
 import {
   Button,
   Header,
@@ -72,7 +77,6 @@ import {
   VISUALLY_HIDDEN,
   WHITE_COLOR,
 } from '../../styles';
-
 import { SETTINGS_STYLES } from './settingsStyles';
 
 function validateColor(color: string): boolean {
@@ -335,7 +339,7 @@ export class Settings extends React.Component<ISettingsProps, ISettingsState> {
         tagId: '1',
         lowerThreshold: .8,
         upperThreshold: 1,
-        action: 'approve',
+        action: RULE_ACTION_ACCEPT,
       },
     );
 
@@ -517,7 +521,7 @@ export class Settings extends React.Component<ISettingsProps, ISettingsState> {
   }
 
   @autobind
-  handleModerateButtonClick(rule: IRuleModel, action: IConfirmationAction) {
+  handleModerateButtonClick(rule: IRuleModel, action: IRuleAction) {
     const updatedRules = this.state.rules.update(
         this.state.rules.findIndex(((r) => r.equals(rule))),
         (r) => r.set('action', action));

packages/frontend-web/src/app/scenes/Settings/components/RuleRow/RuleRow.tsx
@@ -17,8 +17,19 @@ limitations under the License.
 import { autobind } from 'core-decorators';
 import { List } from 'immutable';
 import React from 'react';
-import { ICategoryModel, IPreselectModel, IRuleModel, ITaggingSensitivityModel, ITagModel } from '../../../../../models';
-import { IConfirmationAction } from '../../../../../types';
+import {
+  ICategoryModel,
+  IPreselectModel,
+  IRuleAction,
+  IRuleModel,
+  ITaggingSensitivityModel,
+  ITagModel,
+  RULE_ACTION_ACCEPT,
+  RULE_ACTION_DEFER,
+  RULE_ACTION_HIGHLIGHT,
+  RULE_ACTION_REJECT,
+} from '../../../../../models';
+import { IModerationAction } from '../../../../../types';
 import { ModerateButtons } from '../../../../components';
 import {
   ARTICLE_CATEGORY_TYPE,
@@ -30,8 +41,8 @@ import {
   PALE_COLOR,
 } from '../../../../styles';
 import { maybeCallback, partial } from '../../../../util';
-import { css, stylesheet } from '../../../../utilx';
 import { sortByLabel } from '../../../../util';
+import { css, stylesheet } from '../../../../utilx';
 import { SETTINGS_STYLES } from '../../settingsStyles';
 
 const INPUT_HEIGHT = 36;
@@ -92,11 +103,11 @@ export interface IRuleRowProps {
   tags: List<ITagModel>;
   rangeBottom: number;
   rangeTop: number;
-  selectedAction?: string;
+  selectedAction?: IRuleAction;
   hasTagging?: boolean;
   onModerateButtonClick?(
-    rule: IRuleModel | ITaggingSensitivityModel | IPreselectModel,
-    action: IConfirmationAction,
+    rule: IRuleModel,
+    action: IRuleAction,
   ): any;
   buttons?: JSX.Element;
   selectedCategory: string;
@@ -123,6 +134,32 @@ export class RuleRow extends React.Component<IRuleRowProps> {
     callback(e.target.value);
   }
 
+  @autobind
+  notifyWrapperOfActionChange(action: IModerationAction) {
+    const {
+      onModerateButtonClick,
+      rule,
+    } = this.props;
+    let raction: IRuleAction;
+    switch (action) {
+      case 'approve':
+        raction = RULE_ACTION_ACCEPT;
+        break;
+      case 'reject':
+        raction = RULE_ACTION_REJECT;
+        break;
+      case 'defer':
+        raction = RULE_ACTION_DEFER;
+        break;
+      case 'highlight':
+        raction = RULE_ACTION_HIGHLIGHT;
+        break;
+    }
+
+    if (onModerateButtonClick) {
+      onModerateButtonClick(rule as IRuleModel, raction);
+    }
+  }
   render() {
     const {
       categories,
@@ -130,7 +167,6 @@ export class RuleRow extends React.Component<IRuleRowProps> {
       rangeBottom,
       rangeTop,
       hasTagging,
-      onModerateButtonClick,
       selectedCategory,
       selectedTag,
       selectedAction,
@@ -145,6 +181,18 @@ export class RuleRow extends React.Component<IRuleRowProps> {
     const sortedCategories = sortByLabel(categories);
     const sortedTags = sortByLabel(tags);
 
+    function convertRuleAction(a: IRuleAction) {
+      switch (a) {
+        case RULE_ACTION_ACCEPT:
+          return 'approve';
+        case RULE_ACTION_REJECT:
+          return 'reject';
+        case RULE_ACTION_DEFER:
+          return 'defer';
+        case RULE_ACTION_HIGHLIGHT:
+          return 'highlight';
+      }
+    }
     return (
       <div {...css(STYLES.base, SETTINGS_STYLES.row)}>
         <div {...css(STYLES.selectContainer)}>
@@ -202,10 +250,9 @@ export class RuleRow extends React.Component<IRuleRowProps> {
             <ModerateButtons
               darkOnLight
               hideLabel
-              activeButtons={selectedAction
-                  && List.of(selectedAction.toLowerCase()) as List<IConfirmationAction>}
+              activeButtons={List<IModerationAction>().push(convertRuleAction(selectedAction))}
               containerSize={36}
-              onClick={partial(maybeCallback(onModerateButtonClick), rule)}
+              onClick={this.notifyWrapperOfActionChange}
             />
           )}
         <button

packages/frontend-web/src/app/scenes/Settings/store.ts
@@ -108,7 +108,7 @@ export function updateTags(oldTags: List<ITagModel>, newTags: List<ITagModel>): 
 async function addRule(rule: IRuleModel): Promise<void> {
   await createModel<IRuleModel>(
     'moderation_rules',
-    rule.set('action', rule.action.toLowerCase() === 'approve' ? 'Accept' : rule.action) as any,
+    rule as any,
   );
 }
 
@@ -116,7 +116,7 @@ async function modifyRule(rule: IRuleModel): Promise<void> {
   await updateModel<IRuleModel>(
     'moderation_rules',
     rule.id,
-    rule.set('action', rule.action.toLowerCase() === 'approve' ? 'Accept' : rule.action) as any,
+    rule as any,
   );
 }
 

commit fe3dc41551a17ec6fb86e8fd3a720837a0747693

frontend: Fix for tagging bug introduced by 870d17e2

packages/frontend-web/src/app/scenes/Comments/components/NewComments/NewComments.tsx
@@ -21,12 +21,13 @@ import keyboardJS from 'keyboardjs';
 import React from 'react';
 
 import {
+  convertServerAction,
   IArticleModel,
   ICommentDatedModel,
   ICommentModel,
-  ICommentScoredModel, IServerAction,
+  ICommentScoredModel,
   IRuleModel,
-  ITagModel, SERVER_ACTION_ACCEPT, SERVER_ACTION_DEFER, SERVER_ACTION_HIGHLIGHT, SERVER_ACTION_REJECT,
+  ITagModel,
   TagModel,
 } from '../../../../../models';
 import { ICommentAction } from '../../../../../types';
@@ -301,7 +302,7 @@ export interface INewCommentsState {
   isNavStuck?: boolean;
   isConfirmationModalVisible?: boolean;
   isRuleInfoVisible?: boolean;
-  confirmationAction?: IServerAction;
+  confirmationAction?: ICommentAction;
   actionCount?: number;
   actionText?: string;
   toastButtonLabel?: 'Undo';
@@ -945,16 +946,16 @@ export class NewComments extends React.Component<INewCommentsProps, INewComments
     }
   }
 
-  matchAction(action: IServerAction) {
+  matchAction(action: ICommentAction) {
     let showActionIcon;
 
-    if (action === SERVER_ACTION_ACCEPT) {
+    if (action === 'approve') {
       showActionIcon = <ApproveIcon {...css({ fill: DARK_COLOR })} />;
-    } else if (action === SERVER_ACTION_REJECT) {
+    } else if (action === 'reject') {
       showActionIcon = <RejectIcon {...css({ fill: DARK_COLOR })} />;
-    } else if (action === SERVER_ACTION_HIGHLIGHT) {
+    } else if (action === 'highlight') {
       showActionIcon = <HighlightIcon {...css({ fill: DARK_COLOR })} />;
-    } else if (action === SERVER_ACTION_DEFER) {
+    } else if (action === 'defer') {
       showActionIcon = <DeferIcon {...css({ fill: DARK_COLOR })} />;
     }
 
@@ -962,7 +963,7 @@ export class NewComments extends React.Component<INewCommentsProps, INewComments
   }
 
   @autobind
-  triggerActionToast(action: IServerAction, count: number, callback: (action?: IServerAction) => any) {
+  triggerActionToast(action: ICommentAction, count: number, callback: (action?: ICommentAction) => any) {
     this.setState({
       isConfirmationModalVisible: true,
       confirmationAction: action,
@@ -989,7 +990,7 @@ export class NewComments extends React.Component<INewCommentsProps, INewComments
   }
 
   @autobind handleRemoveAutomatedRule(rule: IRuleModel) {
-    const icon = this.matchAction(rule.action);
+    const icon = this.matchAction(convertServerAction(rule.action));
 
     this.setState({
       isRuleInfoVisible: true,
@@ -1017,7 +1018,7 @@ export class NewComments extends React.Component<INewCommentsProps, INewComments
   @autobind
   onTagButtonClick(tagId: string) {
     const ids = this.getSelectedIDs();
-    this.triggerActionToast('Accept', ids.length, () => this.props.tagComments(ids, tagId));
+    this.triggerActionToast('tag', ids.length, () => this.props.tagComments(ids, tagId));
     this.toggleTaggingToolTip();
   }
 