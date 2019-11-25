f2c89b2eab70687371790dda39a9b72fe6ab95c5
Rufus Hamade <rufus@myfanwy.org.uk>
2019-11-25 08:33:33 +0000

    backend: Separate out serializer code.

---
+++ packages/backend-api/src/api/services/serializer.ts
@@ -0,0 +1,59 @@
+/*
+Copyright 2019 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { pick } from 'lodash';
+import * as Sequelize from 'sequelize';
+
+export const TAG_FIELDS = ['id', 'color', 'description', 'key', 'label', 'isInBatchView', 'inSummaryScore', 'isTaggable'];
+export const RANGE_FIELDS = ['id', 'categoryId', 'lowerThreshold', 'upperThreshold', 'tagId'];
+export const TAGGING_SENSITIVITY_FIELDS = RANGE_FIELDS;
+export const RULE_FIELDS = ['action', 'createdBy', ...RANGE_FIELDS];
+export const PRESELECT_FIELDS = RANGE_FIELDS;
+export const USER_FIELDS = ['id', 'name', 'email', 'avatarURL', 'group', 'isActive'];
+
+const COMMENTSET_FIELDS = ['id', 'updatedAt', 'allCount', 'unprocessedCount', 'unmoderatedCount', 'moderatedCount',
+  'approvedCount', 'highlightedCount', 'rejectedCount', 'deferredCount', 'flaggedCount',
+  'batchedCount', 'recommendedCount', 'assignedModerators', ];
+export const CATEGORY_FIELDS = [...COMMENTSET_FIELDS, 'label', 'ownerId', 'isActive', 'sourceId'];
+export const ARTICLE_FIELDS = [...COMMENTSET_FIELDS, 'title', 'url', 'categoryId', 'sourceCreatedAt', 'lastModeratedAt',
+  'isCommentingEnabled', 'isAutoModerated'];
+
+const ID_FIELDS = new Set(['categoryId', 'tagId', 'ownerId']);
+
+// Convert IDs to strings, and assignedModerators to arrays of strings.
+export function serialiseObject(
+  o: Sequelize.Instance<any>,
+  fields: Array<string>,
+): {[key: string]: {} | Array<string> | string | number} {
+  const serialised = pick(o.toJSON(), fields);
+
+  serialised.id = serialised.id.toString();
+
+  for (const k in serialised) {
+    const v = serialised[k];
+
+    if (ID_FIELDS.has(k) && v) {
+      serialised[k] = v.toString();
+    }
+  }
+
+  if (serialised.assignedModerators) {
+    serialised.assignedModerators = serialised.assignedModerators.map(
+      (i: any) => (i.user_category_assignment ?  i.user_category_assignment.userId.toString() :
+        i.moderator_assignment.userId.toString()));
+  }
+  return serialised;
+}
