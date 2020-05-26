e875968d1efcb30c1d2f4a5087da3c12fdd2becc
Rufus Hamade <rufus@myfanwy.org.uk>
2020-05-26 14:29:28 +0100

    frontend: Modernise ModerateButtons

--- packages/frontend-web/src/app/utilx/hooks.ts
+++ packages/frontend-web/src/app/utilx/hooks.ts
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Google Inc.
+Copyright 2020 Google Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,8 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-export * from './cssInJs';
-export * from './sortDefinitions';
-export * from './highlightText';
-export * from './keyCodes';
-export * from './hooks';
+import keyboardJS from 'keyboardjs';
+import {useEffect} from 'react';
+
+export function useBindEscape(action: () => void) {
+  useEffect(() => {
+    keyboardJS.bind('escape', action);
+    return () => {
+      keyboardJS.unbind('escape', action);
+    };
+  }, []);
+}
